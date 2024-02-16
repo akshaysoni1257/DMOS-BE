2d78aa10a9b696f3f6c793514c1b9c5f27db5725
akshaysoni1257 <112935525+akshaysoni1257@users.noreply.github.com>
2024-02-16 17:43:54 +0530

    Create two diffrent servers admin & customer

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 /* Admin routes */
-app.use('/user',require('./routes/user'));
-app.use('/product',require('./routes/product'));
-app.use('/category',require('./routes/category'));
+// app.use('/user',require('./routes/user'));
+// app.use('/product',require('./routes/product'));
+// app.use('/category',require('./routes/category'));
 
 module.exports = app;
