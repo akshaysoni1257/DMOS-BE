7a77d152a7373af278b46fd653900967847abf84
akshaysoni1257 <112935525+akshaysoni1257@users.noreply.github.com>
2024-04-26 23:44:33 +0530

    Changes in order controller

--- controllers/customer/orderController.js
+++ controllers/customer/orderController.js
@@ -263,7 +263,7 @@ exports.createOrder = async (req, res) => {
 
 exports.viewOrders = async (req, res) => {
     try {
-      const orders = await Order.find({ customer: req.user.userId });
+      const orders = await Order.find({ customer: req.user.userId }).populate('customer', 'first_name last_name email').populate('items.product');
       res.status(200).json(orders);
     } catch (err) {
       res.status(500).json({ message: err.message });
